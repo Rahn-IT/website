cebad0b3e5bb3802ac7b5bd34329b07e2f47aa78
Luca Wlcek <luca@it-rahn.de>
2023-08-30 11:25:30 +0200

    cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,17 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+	import '../app.postcss';
+	import LogoFull from '$lib/images/logo-full.svelte';
 </script>
 
 <div class="app">
-	<Header />
-
+	<nav class="navbar shadow">
+		<div class="navbar-start">
+			<a class="h-16 btn btn-ghost" href="/">
+				<LogoFull />
+			</a>
+		</div>
+	</nav>
 	<main>
 		<slot />
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
