e1a390dffc873ebb4bd862e0d61c8177fa6abd97
Luca Wlcek <luca@it-rahn.de>
2023-09-15 14:30:19 +0200

    tried fixing lcp

--- src/lib/Person.svelte
+++ src/lib/Person.svelte
@@ -9,7 +9,7 @@
 
 <div class="card w-full rounded-3xl bg-base-300 shadow-xl lg:card-side">
 	<figure class="flex-shrink-0 lg:w-1/3">
-		<Img class="h-full w-full rounded-3xl" src={image} alt="Bild von {name}" />
+		<Img class="h-full w-full rounded-3xl" src={image} alt="Bild von {name}" loading="eager" />
 	</figure>
 	<div class="card-body">
 		<h2 class="card-title">{name}</h2>
