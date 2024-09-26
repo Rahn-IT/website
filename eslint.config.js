e0b158f63de00b23e3e2417330d104cfd7ca935f
L. Wlcek <luca@it-rahn.de>
2024-09-26 14:10:33 +0200

    fiex linting and formatting

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+// eslint.config.js
+export default [
+	{
+		ignores: [
+			'.DS_Store',
+			'node_modules',
+			'/build',
+			'/.svelte-kit',
+			'/package',
+			'.env',
+			'.env.*',
+			'!.env.example',
+
+			// Ignore files for PNPM, NPM and YARN
+			'pnpm-lock.yaml',
+			'package-lock.json',
+			'yarn.lock'
+		],
+		rules: {
+			semi: 'error',
+			'prefer-const': 'error'
+		}
+	}
+];
